195c50684f8e3dac912abfd559f7e29dd1d26f02
Tiri <shaymusts@gmail.com>
2022-08-21 20:21:11 +0200

    new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,21 @@ export type RegisterFormValues = {
   registerEmail: string;
   registerPassword: string;
 };
+
+export enum Categories {
+  PRODUCTS = "Products",
+  ENTERTAINMENT = "Entertainment",
+  BILLS = "Bills",
+  OTHER = "Other"
+}
+
+export type CategorySummary = {
+  name: Categories,
+  totalExpense: number,
+}
+
+export type Transaction = {
+  name: string,
+  category: Categories,
+  date: Date
+}
