32f76e56d8e343af595ee631637ecd114e137b33
Tiri <shaymusts@gmail.com>
2022-08-21 20:21:35 +0200

    barrel export for components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as AppNavbar } from "./Navbar"
+export { default as Search } from "./Search"
+export { default as CategoriesSummary } from "./CategoriesSummary"
+export { default as Category } from "./Category"
